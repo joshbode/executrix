bd4608e027fab4317648e8219481c0405ef6a8bf
Josh Bode <joshbode@gmail.com>
2014-11-02 15:26:24 +1100

    added debouncing and press-counts

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,47 +1,87 @@
-/*
-TrinketKeyboard example
-For Trinket by Adafruit Industries
-*/
+// Executrix Button
 
 #include <TrinketKeyboard.h>
 
-#define PIN_BUTTON_CAPITAL_A 0
-#define PIN_BUTTON_STRING    2
+// pins
+const unsigned char BUTTON = 0;
+const unsigned char LED = 1;
 
-void setup()
-{
-  // button pins as inputs
-  pinMode(PIN_BUTTON_CAPITAL_A, INPUT);
-  pinMode(PIN_BUTTON_STRING, INPUT);
+// debouncing
+const unsigned short DEBOUNCE_DELAY = 50;
+unsigned long debounce_expiry = 0;
+bool button_state = HIGH, prev_reading = HIGH;
 
-  // setting input pins to high means turning on internal pull-up resistors
-  digitalWrite(PIN_BUTTON_CAPITAL_A, HIGH);
-  digitalWrite(PIN_BUTTON_STRING, HIGH);
-  // remember, the buttons are active-low, they read LOW when they are not pressed
+// press counting
+const unsigned short COUNT_DELAY = 500;
+unsigned long count_expiry = 0;
+const unsigned short MAX_COUNT = 4;
+unsigned short count = 0;
 
-  // start USB stuff
+
+// setup button
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, HIGH);
+
   TrinketKeyboard.begin();
 }
 
-void loop()
-{
+
+// main loop
+void loop() {
   TrinketKeyboard.poll();
-  // the poll function must be called at least once every 10 ms
-  // or cause a keystroke
-  // if it is not, then the computer may think that the device
-  // has stopped working, and give errors
 
-  if (digitalRead(PIN_BUTTON_CAPITAL_A) == LOW)
-  {
-    TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_A);
-    // this should type a capital A
-    TrinketKeyboard.pressKey(0, 0);
-    // this releases the key
-  }
+  unsigned long time = millis();
 
-  if (digitalRead(PIN_BUTTON_STRING) == LOW)
-  {
-    // type out a string using the Print class
-    TrinketKeyboard.print("Hello World!");
+  // process if outside of sample window
+  if (count && (time > count_expiry)) {
+    switch (count) {
+      case 1:
+        TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_APP);
+        break;
+      case 2:
+        TrinketKeyboard.pressKey(KEYCODE_MOD_RIGHT_SHIFT, KEYCODE_APP);
+        break;
+      case 3:
+        TrinketKeyboard.pressKey(KEYCODE_MOD_RIGHT_ALT | KEYCODE_MOD_RIGHT_CONTROL, KEYCODE_DELETE);
+        break;
+      default:
+        break;
+    }
+    TrinketKeyboard.pressKey(0, 0);  // lift key
+
+    //TrinketKeyboard.print(count);
+    //TrinketKeyboard.print("\n");
+
+    // reset count
+    count = 0;
+  } else {
+    bool reading = digitalRead(BUTTON);
+    digitalWrite(LED, !reading);
+
+    // debounce reading
+    if (reading != prev_reading) {
+      debounce_expiry = time + DEBOUNCE_DELAY;
+    } else if (time > debounce_expiry) {
+      if (reading != button_state) {
+        if (reading == LOW) {
+          if (count < MAX_COUNT) {
+            // increment count and reset sample window
+            count++;
+            count_expiry = time + COUNT_DELAY;
+          } else {
+            // max presses reached so invalidate window
+            count_expiry = 0;
+          }
+        }
+
+        button_state = reading;
+      } else {
+
+      }
+    }
+
+    prev_reading = reading;
   }
 }
